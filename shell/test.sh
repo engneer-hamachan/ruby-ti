86a1948fbc0ee6fd570d04b89a7f48a412aa5cca
hamachan <yusuke9127@gmail.com>
2025-09-03 06:28:22 +0900

    fix: test command

--- shell/test.sh
+++ shell/test.sh
@@ -1,2 +1,1 @@
-# go build -o ti main.go && go test ./test/... -v -count=1 && rm ti
-go build -pgo ./cpu.pprof -o ti main.go && go test ./test/... -v -count=1 && rm ti
+go build -o ti main.go && go test ./test/... -v -count=1 && rm ti
