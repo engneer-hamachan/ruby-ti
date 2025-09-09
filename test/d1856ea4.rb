e568c1b3126e031fa5da3505d01b4ad787c00c4d
hamachan <yusuke9127@gmail.com>
2025-09-10 08:43:19 +0900

    fix: parse error

---
+++ test/d1856ea4.rb
@@ -0,0 +1,8 @@
+multiline = <<EOF
+multiline string
+EOF
+
+dbtp multiline
+
+complex_result = (x + y) 
+dbtp 1
---
+++ test/d1856ea4_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"os/exec"
+	"strings"
+	"testing"
+)
+
+func TestD1856ea4(t *testing.T) {
+	cmd := exec.Command("../ti", "./d1856ea4.rb")
+
+	output, _ := cmd.CombinedOutput()
+
+	expectedOutput := `./d1856ea4.rb::5::String
+./d1856ea4.rb::7::method '+' is not defined for Identifier
+./d1856ea4.rb::8::Integer`
+
+	if strings.TrimSpace(string(output)) != strings.TrimSpace(expectedOutput) {
+		t.Errorf("Expected output: %s, but got: %s", expectedOutput, string(output))
+	}
+}
