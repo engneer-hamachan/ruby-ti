e08a13aa280a225fca0dc0446b2706edd3bf003c
hamachan <yusuke9127@gmail.com>
2025-09-12 07:56:34 +0900

    fix:  p 1, 2, 3

---
+++ test/03d9258d_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"os/exec"
+	"strings"
+	"testing"
+)
+
+func Test03d9258d(t *testing.T) {
+	cmd := exec.Command("../ti", "./03d9258d.rb")
+
+	output, _ := cmd.CombinedOutput()
+
+	expectedOutput := `./03d9258d.rb::1::Array<Integer>`
+
+	if strings.TrimSpace(string(output)) != strings.TrimSpace(expectedOutput) {
+		t.Errorf("Expected output: %s, but got: %s", expectedOutput, string(output))
+	}
+}
