75c332e36b54751dbb8b38706b27ca535e7eec7d
hamachan <yusuke9127@gmail.com>
2025-09-23 06:09:03 +0900

    fix: Proc & yield proccess

---
+++ test/a3fa7d9f_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"os/exec"
+	"strings"
+	"testing"
+)
+
+func TestA3fa7d9f(t *testing.T) {
+	cmd := exec.Command("../ti", "./a3fa7d9f.rb")
+
+	output, _ := cmd.CombinedOutput()
+
+	expectedOutput := `./a3fa7d9f.rb::18::Integer`
+
+	if strings.TrimSpace(string(output)) != strings.TrimSpace(expectedOutput) {
+		t.Errorf("Expected output: %s, but got: %s", expectedOutput, string(output))
+	}
+}
