9ee10aaad19605cb789748acbbc638302099e867
hamachan <yusuke9127@gmail.com>
2025-10-01 08:31:13 +0900

    add: rbs to buitin json generator

---
+++ rb_tools/rbs_to_json.rb
@@ -0,0 +1,253 @@
+#!/usr/bin/env ruby
+require 'rbs'
+require 'json'
+
+######WARNING!!!#######
+#this program is WIP!!#
+#######################
+
+def extract_type(type)
+  case type
+  when RBS::Types::Bases::Self
+    "Self"
+  when RBS::Types::Bases::Bool
+    "Bool"
+  when RBS::Types::Bases::Instance
+    "Self"
+  when RBS::Types::Bases::Void
+    "Void"
+  when RBS::Types::Bases::Any
+    "Untyped"
+  when RBS::Types::ClassInstance
+    case type.name.name.to_s
+    when "Array"
+      "Array"
+    when "Integer"
+      "Int"
+    when "String"
+      "String"
+    else
+      type.name.name.to_s
+    end
+  when RBS::Types::Optional
+    inner = extract_type(type.type)
+    "Optional#{inner}"
+  when RBS::Types::Union
+    return type.types.map { |t| extract_type(t) }
+  when RBS::Types::Variable
+    # Elem -> Unify
+    "Unify"
+  when RBS::Types::Alias
+    case type.name.name.to_s
+    when "int"
+      "Int"
+    when "string"
+      "String"
+    when "bool"
+      "Bool"
+    when "boolish"
+      "Bool"
+    else
+      "Untyped"
+    end
+  else
+    "Untyped"
+  end
+end
+
+def normalize_type(type)
+  result = extract_type(type)
+  result.is_a?(Array) ? result : [result]
+end
+
+def is_optional_param?(param_name)
+  param_name && param_name.to_s.start_with?('?')
+end
+
+# parsing rbs
+input_file = ARGV[0] || 'input_sample.rbs'
+buffer = RBS::Buffer.new(name: input_file, content: File.read(input_file))
+result = RBS::Parser.parse_signature(buffer)
+
+# result [buffer, _, declarations]
+decls = result[2]
+
+# find first class
+klass_decl = decls.find { |d| d.is_a?(RBS::AST::Declarations::Class) }
+
+unless klass_decl
+  puts "No class declaration found"
+  exit 1
+end
+
+# merge overload methods
+methods_map = {}
+method_order = []
+
+klass_decl.members.each do |member|
+  case member
+  when RBS::AST::Members::MethodDefinition
+    next unless member.instance?
+
+    method_name = member.name.to_s
+    if !methods_map.key?(method_name)
+      methods_map[method_name] = []
+      method_order << method_name
+    end
+    methods_map[method_name] << member
+  end
+end
+
+# collect instance methods
+instance_methods = []
+
+method_order.each do |method_name|
+  members = methods_map[method_name]
+  # collect all overloads
+  all_overloads = members.flat_map(&:overloads)
+
+  next if all_overloads.empty?
+
+  method_data = {
+    "name" => method_name
+  }
+
+  # merge
+  all_arg_types = []
+  all_return_types = []
+  block_params = nil
+  has_optional_args = false
+
+  all_overloads.each do |overload|
+    # 引数の型を収集
+    arg_types = []
+
+    overload.method_type.type.required_positionals.each do |param|
+      arg_types << normalize_type(param.type)
+    end
+
+    overload.method_type.type.optional_positionals.each do |param|
+      arg_types << normalize_type(param.type)
+      has_optional_args = true
+    end
+
+    if overload.method_type.type.rest_positionals
+      arg_types << ["Untyped"]
+    end
+
+    all_arg_types << arg_types unless arg_types.empty?
+
+    # collect return type
+    return_type = normalize_type(overload.method_type.type.return_type)
+    all_return_types << return_type
+
+    # collect block params
+    if overload.method_type.block && !block_params
+      block_params = overload.method_type.block.type.required_positionals.map do |param|
+        extract_type(param.type)
+      end
+      block_params = [block_params].flatten
+    end
+  end
+
+  # merge arguments
+  if all_arg_types.any?
+    # 引数の数が最大のものを基準に
+    max_args = all_arg_types.max_by(&:size)
+
+    arguments = []
+    max_args.each_with_index do |arg_type, idx|
+      # calculate overload
+      merged_types = all_arg_types.map { |args| args[idx] }.compact.flatten.uniq
+
+      # isDefault
+      if has_optional_args && idx >= (max_args.size - 1)
+        merged_types = merged_types.map { |t| t.start_with?("Default") ? t : "Default#{t}" }
+      end
+
+      arguments << {"type" => merged_types}
+    end
+
+    method_data["arguments"] = arguments unless arguments.empty?
+  else
+    method_data["arguments"] = []
+  end
+
+  # insert block params
+  if block_params && !block_params.empty?
+    method_data["block_parameters"] = block_params
+  end
+
+  # merge return type
+  merged_return_types = all_return_types.flatten.uniq
+  return_type_data = {"type" => merged_return_types}
+
+  # is_conditional
+  if merged_return_types.size > 1
+    return_type_data["is_conditional"] = true
+  end
+
+  # is_destructive
+  if method_name.end_with?("!")
+    return_type_data["is_destructive"] = true
+  end
+
+  method_data["return_type"] = return_type_data
+
+  instance_methods << method_data
+end
+
+# collect class methods
+class_methods = []
+klass_decl.members.each do |member|
+  case member
+  when RBS::AST::Members::MethodDefinition
+    next unless member.singleton?
+
+    method_data = {
+      "name" => member.name.to_s
+    }
+
+    if member.overloads.any?
+      overload = member.overloads.first
+      args = []
+
+      overload.method_type.type.required_positionals.each do |param|
+        args << {"type" => normalize_type(param.type)}
+      end
+
+      overload.method_type.type.optional_positionals.each do |param|
+        types = normalize_type(param.type)
+        args << {"type" => types.map { |t| "Default#{t}" }}
+      end
+
+      method_data["arguments"] = args unless args.empty?
+
+      return_type = normalize_type(overload.method_type.type.return_type)
+      method_data["return_type"] = {"type" => return_type}
+    end
+
+    class_methods << method_data
+  end
+end
+
+# extends
+extends = []
+klass_decl.members.each do |member|
+  case member
+  when RBS::AST::Members::Include
+    extends << member.name.name.to_s
+  end
+end
+
+output = {
+  "frame" => "Builtin",
+  "class" => klass_decl.name.name.to_s
+}
+
+output["extends"] = extends unless extends.empty?
+output["instance_methods"] = instance_methods
+output["class_methods"] = class_methods unless class_methods.empty?
+
+# output
+puts JSON.pretty_generate(output)
