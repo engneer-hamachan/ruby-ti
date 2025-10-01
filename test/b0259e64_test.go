8e22d2095c909a632c5bf904ba53bcc2a94119c1
hamachan <yusuke9127@gmail.com>
2025-10-02 08:30:15 +0900

    add: return nested class in builtin types

---
+++ test/b0259e64_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"os/exec"
+	"strings"
+	"testing"
+)
+
+func TestB0259e64(t *testing.T) {
+	cmd := exec.Command("../ti", "./b0259e64.rb")
+
+	output, _ := cmd.CombinedOutput()
+
+	expectedOutput := `./b0259e64.rb::17::Integer`
+
+	if strings.TrimSpace(string(output)) != strings.TrimSpace(expectedOutput) {
+		t.Errorf("Expected output: %s, but got: %s", expectedOutput, string(output))
+	}
+}
