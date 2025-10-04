7a85d656b21a96fa1e7a373a879b8140c9d495fd
hamachan <yusuke9127@gmail.com>
2025-10-05 08:17:01 +0900

    add: rbs test generator

--- shell/generate_test_from_rbs.sh
+++ shell/generate_test_from_rbs.sh
@@ -34,10 +34,57 @@ cat > "$TEST_FILE" << GOEOF
 package main
 
 import (
+	"fmt"
 	"os/exec"
 	"strings"
 	"testing"
 )
+
+// showDiff displays a diff by finding the first difference in the strings
+func showDiff(expected, actual string) string {
+	var diff strings.Builder
+	diff.WriteString("\n=== DIFF ===\n")
+
+	// Find first difference
+	minLen := len(expected)
+	if len(actual) < minLen {
+		minLen = len(actual)
+	}
+
+	for i := 0; i < minLen; i++ {
+		if expected[i] != actual[i] {
+			start := i - 40
+			if start < 0 {
+				start = 0
+			}
+			end := i + 60
+			expEnd := end
+			actEnd := end
+			if expEnd > len(expected) {
+				expEnd = len(expected)
+			}
+			if actEnd > len(actual) {
+				actEnd = len(actual)
+			}
+
+			diff.WriteString(fmt.Sprintf("First difference at position %d:\n", i))
+			diff.WriteString(fmt.Sprintf("Expected: ...%s...\n", expected[start:expEnd]))
+			diff.WriteString(fmt.Sprintf("Got:      ...%s...\n", actual[start:actEnd]))
+			return diff.String()
+		}
+	}
+
+	if len(expected) != len(actual) {
+		diff.WriteString(fmt.Sprintf("Lengths differ: expected %d, got %d\n", len(expected), len(actual)))
+		if len(expected) > minLen {
+			diff.WriteString(fmt.Sprintf("Expected has extra: %s\n", expected[minLen:]))
+		} else {
+			diff.WriteString(fmt.Sprintf("Got has extra: %s\n", actual[minLen:]))
+		}
+	}
+
+	return diff.String()
+}
 
 func Test_RBS_${BASENAME}_${HASH}(t *testing.T) {
 	cmd := exec.Command("sh", "./shell/rbs_to_json_test.sh", "${RBS_FILE}")
