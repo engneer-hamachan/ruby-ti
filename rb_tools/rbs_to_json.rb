a2dd4cff6b9f99d824f0b11a8941ca6a78ed13b0
hamachan <yusuke9127@gmail.com>
2025-10-06 08:08:08 +0900

    fix: refact rbs_to_json

--- rb_tools/rbs_to_json.rb
+++ rb_tools/rbs_to_json.rb
@@ -6,53 +6,47 @@ require 'json'
 #this program is WIP!!#
 #######################
 
+# Type conversion functions
 def extract_type(type)
   case type
-  when RBS::Types::Bases::Self
+  when RBS::Types::Bases::Self, RBS::Types::Bases::Instance
     "Self"
   when RBS::Types::Bases::Bool
     "Bool"
-  when RBS::Types::Bases::Instance
-    "Self"
   when RBS::Types::Bases::Void
     "Nil"
   when RBS::Types::Bases::Any
     "Untyped"
   when RBS::Types::ClassInstance
-    case type.name.name.to_s
-    when "Array"
-      "Array"
-    when "Integer"
-      "Int"
-    when "String"
-      "String"
-    else
-      # Handle namespaced classes (e.g., JS::Object)
-      type.name.to_s
-    end
+    extract_class_instance_type(type)
   when RBS::Types::Optional
-    inner = extract_type(type.type)
-    "Optional#{inner}"
+    "Optional#{extract_type(type.type)}"
   when RBS::Types::Union
-    return type.types.map { |t| extract_type(t) }
+    type.types.map { |t| extract_type(t) }
   when RBS::Types::Variable
-    # Elem -> Unify
     "Unify"
   when RBS::Types::Alias
-    case type.name.name.to_s
-    when "int"
-      "Int"
-    when "string"
-      "String"
-    when "bool"
-      "Bool"
-    when "boolish"
-      "Bool"
-    else
-      "Untyped"
-    end
+    extract_alias_type(type)
   else
     "Untyped"
+  end
+end
+
+def extract_class_instance_type(type)
+  case type.name.name.to_s
+  when "Array" then "Array"
+  when "Integer" then "Int"
+  when "String" then "String"
+  else type.name.to_s
+  end
+end
+
+def extract_alias_type(type)
+  case type.name.name.to_s
+  when "int" then "Int"
+  when "string" then "String"
+  when "bool", "boolish" then "Bool"
+  else "Untyped"
   end
 end
 
@@ -61,326 +55,326 @@ def normalize_type(type)
   result.is_a?(Array) ? result : [result]
 end
 
-def is_optional_param?(param_name)
-  param_name && param_name.to_s.start_with?('?')
+# Argument processing functions
+def process_required_args(required_positionals)
+  required_positionals.map { |param| { "type" => normalize_type(param.type) } }
 end
 
-# parsing rbs
-input_file = ARGV[0] || 'input_sample.rbs'
-buffer = RBS::Buffer.new(name: input_file, content: File.read(input_file))
-result = RBS::Parser.parse_signature(buffer)
-
-# result [buffer, _, declarations]
-decls = result[2]
-
-# find module or class
-module_decl = decls.find { |d| d.is_a?(RBS::AST::Declarations::Module) }
-class_decls = []
-frame_name = "Builtin"
-module_class_methods = []
-
-if module_decl
-  # extract all classes from module
-  frame_name = module_decl.name.name.to_s
-  class_decls = module_decl.members.select { |m| m.is_a?(RBS::AST::Declarations::Class) }
-
-  # extract module's singleton methods (self.xxx methods)
-  module_decl.members.each do |member|
-    if member.is_a?(RBS::AST::Members::MethodDefinition) && member.singleton?
-      module_class_methods << member
-    end
+def process_optional_args(optional_positionals)
+  optional_positionals.map do |param|
+    types = normalize_type(param.type)
+    { "type" => types.map { |t| "Default#{t}" } }
   end
-else
-  # find standalone classes
-  class_decls = decls.select { |d| d.is_a?(RBS::AST::Declarations::Class) }
 end
 
-if class_decls.empty? && module_class_methods.empty?
-  puts "No class declaration found"
-  exit 1
+def process_arguments(method_type)
+  args = []
+  args.concat(process_required_args(method_type.required_positionals))
+  args.concat(process_optional_args(method_type.optional_positionals))
+  args << { "type" => ["Untyped"] } if method_type.rest_positionals
+  args
 end
 
-# process all classes
-all_outputs = []
+def extract_block_params(method_type)
+  return nil unless method_type.block
 
-# If module has singleton methods, add module as a class
-if module_decl && module_class_methods.any?
-  module_output = {
+  params = method_type.block.type.required_positionals.map do |param|
+    extract_type(param.type)
+  end
+  [params].flatten
+end
+
+# Module method collection
+def collect_module_singleton_methods(module_decl)
+  methods = []
+  module_decl.members.each do |member|
+    next unless member.is_a?(RBS::AST::Members::MethodDefinition) && member.singleton?
+    methods << member
+  end
+  methods
+end
+
+def build_module_method(member)
+  return nil unless member.overloads.any?
+
+  overload = member.overloads.first
+  args = process_arguments(overload.method_type.type)
+  return_type = normalize_type(overload.method_type.type.return_type)
+
+  method_data = { "name" => member.name.to_s }
+  method_data["arguments"] = args unless args.empty?
+  method_data["return_type"] = { "type" => return_type }
+  method_data
+end
+
+def build_module_output(module_decl, module_methods)
+  return nil if module_methods.empty?
+
+  {
     "frame" => "Builtin",
     "class" => module_decl.name.name.to_s,
     "instance_methods" => [],
-    "class_methods" => []
+    "class_methods" => module_methods.compact
   }
-
-  # Process module's singleton methods as class methods
-  module_class_methods.each do |member|
-    method_data = {
-      "name" => member.name.to_s
-    }
-
-    if member.overloads.any?
-      overload = member.overloads.first
-      args = []
-
-      overload.method_type.type.required_positionals.each do |param|
-        args << {"type" => normalize_type(param.type)}
-      end
-
-      overload.method_type.type.optional_positionals.each do |param|
-        types = normalize_type(param.type)
-        args << {"type" => types.map { |t| "Default#{t}" }}
-      end
-
-      method_data["arguments"] = args unless args.empty?
-
-      return_type = normalize_type(overload.method_type.type.return_type)
-      method_data["return_type"] = {"type" => return_type}
-    end
-
-    module_output["class_methods"] << method_data
-  end
-
-  all_outputs << module_output
 end
 
-class_decls.each do |klass_decl|
-  # merge overload methods
+# RBS parsing and declaration extraction
+def parse_rbs_file(file_path)
+  buffer = RBS::Buffer.new(name: file_path, content: File.read(file_path))
+  result = RBS::Parser.parse_signature(buffer)
+  result[2] # declarations
+end
+
+def extract_module_and_classes(decls)
+  module_decl = decls.find { |d| d.is_a?(RBS::AST::Declarations::Module) }
+
+  if module_decl
+    frame_name = module_decl.name.name.to_s
+    class_decls = module_decl.members.select { |m| m.is_a?(RBS::AST::Declarations::Class) }
+  else
+    frame_name = "Builtin"
+    class_decls = decls.select { |d| d.is_a?(RBS::AST::Declarations::Class) }
+  end
+
+  { module: module_decl, classes: class_decls, frame: frame_name }
+end
+
+# Instance method collection
+def group_instance_methods(klass_decl)
   methods_map = {}
   method_order = []
 
   klass_decl.members.each do |member|
-    case member
-    when RBS::AST::Members::MethodDefinition
-      next unless member.instance?
-      next if member.visibility == :private
-      next if member.name.to_s == "initialize"  # skip initialize, handle as class method 'new'
+    next unless member.is_a?(RBS::AST::Members::MethodDefinition)
+    next unless member.instance?
+    next if member.visibility == :private
+    next if member.name.to_s == "initialize"
 
-      method_name = member.name.to_s
-      if !methods_map.key?(method_name)
-        methods_map[method_name] = []
-        method_order << method_name
-      end
-      methods_map[method_name] << member
+    method_name = member.name.to_s
+    unless methods_map.key?(method_name)
+      methods_map[method_name] = []
+      method_order << method_name
     end
+    methods_map[method_name] << member
   end
 
-  # collect instance methods
+  { map: methods_map, order: method_order }
+end
+
+def merge_method_overloads(method_name, all_overloads)
+  all_arg_types = []
+  all_return_types = []
+  block_params = nil
+  has_optional_args = false
+
+  all_overloads.each do |overload|
+    method_type = overload.method_type.type
+    arg_types = []
+
+    # Collect argument types
+    method_type.required_positionals.each { |p| arg_types << normalize_type(p.type) }
+    method_type.optional_positionals.each do |p|
+      arg_types << normalize_type(p.type)
+      has_optional_args = true
+    end
+    arg_types << ["Untyped"] if method_type.rest_positionals
+
+    all_arg_types << arg_types unless arg_types.empty?
+    all_return_types << normalize_type(method_type.return_type)
+    block_params ||= extract_block_params(overload.method_type)
+  end
+
+  build_instance_method_data(method_name, all_arg_types, all_return_types, block_params, has_optional_args)
+end
+
+def build_instance_method_data(method_name, all_arg_types, all_return_types, block_params, has_optional_args)
+  method_data = { "name" => method_name }
+
+  # Merge arguments
+  if all_arg_types.any?
+    max_args = all_arg_types.max_by(&:size)
+    arguments = []
+
+    max_args.each_with_index do |arg_type, idx|
+      merged_types = all_arg_types.map { |args| args[idx] }.compact.flatten.uniq
+
+      if has_optional_args && idx >= (max_args.size - 1)
+        merged_types = merged_types.map { |t| t.start_with?("Default") ? t : "Default#{t}" }
+      end
+
+      arguments << { "type" => merged_types }
+    end
+
+    method_data["arguments"] = arguments unless arguments.empty?
+  else
+    method_data["arguments"] = []
+  end
+
+  # Add block parameters
+  method_data["block_parameters"] = block_params if block_params && !block_params.empty?
+
+  # Build return type
+  merged_return_types = all_return_types.flatten.uniq
+  return_data = { "type" => merged_return_types }
+  return_data["is_conditional"] = true if merged_return_types.size > 1
+  return_data["is_destructive"] = true if method_name.end_with?("!")
+  method_data["return_type"] = return_data
+
+  method_data
+end
+
+def collect_instance_methods(klass_decl)
+  grouped = group_instance_methods(klass_decl)
   instance_methods = []
 
-  method_order.each do |method_name|
-    members = methods_map[method_name]
-    # collect all overloads
+  grouped[:order].each do |method_name|
+    members = grouped[:map][method_name]
     all_overloads = members.flat_map(&:overloads)
-
     next if all_overloads.empty?
 
-    method_data = {
-      "name" => method_name
-    }
-
-    # merge
-    all_arg_types = []
-    all_return_types = []
-    block_params = nil
-    has_optional_args = false
-
-    all_overloads.each do |overload|
-      # 引数の型を収集
-      arg_types = []
-
-      overload.method_type.type.required_positionals.each do |param|
-        arg_types << normalize_type(param.type)
-      end
-
-      overload.method_type.type.optional_positionals.each do |param|
-        arg_types << normalize_type(param.type)
-        has_optional_args = true
-      end
-
-      if overload.method_type.type.rest_positionals
-        arg_types << ["Untyped"]
-      end
-
-      all_arg_types << arg_types unless arg_types.empty?
-
-      # collect return type
-      return_type = normalize_type(overload.method_type.type.return_type)
-      all_return_types << return_type
-
-      # collect block params
-      if overload.method_type.block && !block_params
-        block_params = overload.method_type.block.type.required_positionals.map do |param|
-          extract_type(param.type)
-        end
-        block_params = [block_params].flatten
-      end
-    end
-
-    # merge arguments
-    if all_arg_types.any?
-      # 引数の数が最大のものを基準に
-      max_args = all_arg_types.max_by(&:size)
-
-      arguments = []
-      max_args.each_with_index do |arg_type, idx|
-        # calculate overload
-        merged_types = all_arg_types.map { |args| args[idx] }.compact.flatten.uniq
-
-        # isDefault
-        if has_optional_args && idx >= (max_args.size - 1)
-          merged_types = merged_types.map { |t| t.start_with?("Default") ? t : "Default#{t}" }
-        end
-
-        arguments << {"type" => merged_types}
-      end
-
-      method_data["arguments"] = arguments unless arguments.empty?
-    else
-      method_data["arguments"] = []
-    end
-
-    # insert block params
-    if block_params && !block_params.empty?
-      method_data["block_parameters"] = block_params
-    end
-
-    # merge return type
-    merged_return_types = all_return_types.flatten.uniq
-    return_type_data = {"type" => merged_return_types}
-
-    # is_conditional
-    if merged_return_types.size > 1
-      return_type_data["is_conditional"] = true
-    end
-
-    # is_destructive
-    if method_name.end_with?("!")
-      return_type_data["is_destructive"] = true
-    end
-
-    method_data["return_type"] = return_type_data
-
+    method_data = merge_method_overloads(method_name, all_overloads)
     instance_methods << method_data
   end
 
-  # collect class methods
-  class_methods = []
+  instance_methods
+end
 
-  # find initialize method and convert to 'new'
-  initialize_method = klass_decl.members.find do |member|
+# Class method collection
+def find_initialize_method(klass_decl)
+  klass_decl.members.find do |member|
     member.is_a?(RBS::AST::Members::MethodDefinition) &&
     member.instance? &&
     member.name.to_s == "initialize"
   end
+end
 
-  if initialize_method && initialize_method.overloads.any?
-    overload = initialize_method.overloads.first
-    args = []
+def build_new_method(klass_decl, initialize_method)
+  return nil unless initialize_method&.overloads&.any?
 
-    overload.method_type.type.required_positionals.each do |param|
-      args << {"type" => normalize_type(param.type)}
-    end
+  overload = initialize_method.overloads.first
+  args = process_arguments(overload.method_type.type)
 
-    overload.method_type.type.optional_positionals.each do |param|
-      types = normalize_type(param.type)
-      args << {"type" => types.map { |t| "Default#{t}" }}
-    end
+  {
+    "name" => "new",
+    "arguments" => args,
+    "return_type" => { "type" => [klass_decl.name.name.to_s] }
+  }
+end
 
-    new_method = {
-      "name" => "new",
-      "arguments" => args,
-      "return_type" => {"type" => [klass_decl.name.name.to_s]}
-    }
-    class_methods << new_method
-  end
+def collect_singleton_methods(klass_decl)
+  methods = []
 
-  # collect other class methods
   klass_decl.members.each do |member|
-    case member
-    when RBS::AST::Members::MethodDefinition
-      next unless member.singleton?
-      next if member.visibility == :private
+    next unless member.is_a?(RBS::AST::Members::MethodDefinition)
+    next unless member.singleton?
+    next if member.visibility == :private
+    next unless member.overloads.any?
 
-      method_data = {
-        "name" => member.name.to_s
-      }
+    overload = member.overloads.first
+    args = process_arguments(overload.method_type.type)
+    return_type = normalize_type(overload.method_type.type.return_type)
 
-      if member.overloads.any?
-        overload = member.overloads.first
-        args = []
-
-        overload.method_type.type.required_positionals.each do |param|
-          args << {"type" => normalize_type(param.type)}
-        end
-
-        overload.method_type.type.optional_positionals.each do |param|
-          types = normalize_type(param.type)
-          args << {"type" => types.map { |t| "Default#{t}" }}
-        end
-
-        method_data["arguments"] = args unless args.empty?
-
-        return_type = normalize_type(overload.method_type.type.return_type)
-        method_data["return_type"] = {"type" => return_type}
-      end
-
-      class_methods << method_data
-    end
+    method_data = { "name" => member.name.to_s }
+    method_data["arguments"] = args unless args.empty?
+    method_data["return_type"] = { "type" => return_type }
+    methods << method_data
   end
 
-  # extends
+  methods
+end
+
+def collect_class_methods(klass_decl)
+  methods = []
+  initialize_method = find_initialize_method(klass_decl)
+  new_method = build_new_method(klass_decl, initialize_method)
+  methods << new_method if new_method
+  methods.concat(collect_singleton_methods(klass_decl))
+  methods
+end
+
+# Class extends collection
+def collect_extends(klass_decl)
   extends = []
   klass_decl.members.each do |member|
-    case member
-    when RBS::AST::Members::Include
-      extends << member.name.name.to_s
-    end
+    extends << member.name.name.to_s if member.is_a?(RBS::AST::Members::Include)
   end
+  extends
+end
 
+# Build class output
+def build_class_output(klass_decl, frame_name)
   output = {
     "frame" => frame_name,
     "class" => klass_decl.name.name.to_s
   }
 
+  extends = collect_extends(klass_decl)
   output["extends"] = extends unless extends.empty?
-  output["instance_methods"] = instance_methods
+
+  output["instance_methods"] = collect_instance_methods(klass_decl)
+
+  class_methods = collect_class_methods(klass_decl)
   output["class_methods"] = class_methods unless class_methods.empty?
 
-  all_outputs << output
+  output
 end
 
+# Output handling
+def generate_filename(frame, class_name)
+  if frame == "Builtin"
+    "#{class_name.downcase}.json"
+  else
+    "#{frame.downcase}_#{class_name.downcase}.json"
+  end
+end
 
-if ARGV.include?("--test")
+def output_to_files(all_outputs, output_dir = 'myjson')
+  Dir.mkdir(output_dir) unless Dir.exist?(output_dir)
+
+  all_outputs.each do |output|
+    filename = generate_filename(output["frame"], output["class"])
+    filepath = File.join(output_dir, filename)
+    File.write(filepath, JSON.pretty_generate(output))
+    puts "Generated #{filepath}"
+  end
+
+  puts "\nAll JSON files generated in #{output_dir}/ directory"
+end
+
+def output_to_console(all_outputs)
   all_outputs.each do |output|
     p JSON.pretty_generate(output)
   end
-
-  return
 end
 
-# Create output directory
-output_dir = 'myjson'
-Dir.mkdir(output_dir) unless Dir.exist?(output_dir)
+# Main execution
+input_file = ARGV[0] || 'input_sample.rbs'
+decls = parse_rbs_file(input_file)
+parsed = extract_module_and_classes(decls)
 
-# Output each class to a separate file
-all_outputs.each do |output|
-  # Generate filename from frame and class name
-  frame = output["frame"]
-  class_name = output["class"]
-
-  # Create a clean filename
-  if frame == "Builtin"
-    filename = "#{class_name.downcase}.json"
-  else
-    # For namespaced classes like JS::Object -> js_object.json
-    filename = "#{frame.downcase}_#{class_name.downcase}.json"
-  end
-
-  filepath = File.join(output_dir, filename)
-
-  # Write to file
-  File.write(filepath, JSON.pretty_generate(output))
-  puts "Generated #{filepath}"
+if parsed[:classes].empty? && (!parsed[:module] || collect_module_singleton_methods(parsed[:module]).empty?)
+  puts "No class declaration found"
+  exit 1
 end
 
-puts "\nAll JSON files generated in #{output_dir}/ directory"
+all_outputs = []
+
+# Process module singleton methods if present
+if parsed[:module]
+  module_methods = collect_module_singleton_methods(parsed[:module])
+  module_methods_data = module_methods.map { |m| build_module_method(m) }
+  module_output = build_module_output(parsed[:module], module_methods_data)
+  all_outputs << module_output if module_output
+end
+
+# Process all classes
+parsed[:classes].each do |klass_decl|
+  all_outputs << build_class_output(klass_decl, parsed[:frame])
+end
+
+# Execute output
+if ARGV.include?("--test")
+  output_to_console(all_outputs)
+else
+  output_to_files(all_outputs)
+end
