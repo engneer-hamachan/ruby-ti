03f8e1ac80e78df70b4eb1f6faa9165d456ab695
hamachan <yusuke9127@gmail.com>
2025-10-13 07:18:25 +0900

    tmp

--- lsp/server.go
+++ lsp/server.go
@@ -38,26 +38,28 @@ func initialize(
 
 	capabilities.CompletionProvider = &protocol.CompletionOptions{
 		TriggerCharacters: []string{
-			".", "@", ":", "[",
 			"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
 			"n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
 			"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
 			"N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-			"_", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+			"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+			".", "_",
 		},
 	}
 
 	syncKind := protocol.TextDocumentSyncKindFull
-	capabilities.TextDocumentSync = protocol.TextDocumentSyncOptions{
-		OpenClose: &[]bool{true}[0],
-		Change:    &syncKind,
-		Save:      &protocol.SaveOptions{IncludeText: &[]bool{true}[0]},
-	}
+
+	capabilities.TextDocumentSync =
+		protocol.TextDocumentSyncOptions{
+			OpenClose: &[]bool{true}[0],
+			Change:    &syncKind,
+			Save:      &protocol.SaveOptions{IncludeText: &[]bool{true}[0]},
+		}
 
 	return protocol.InitializeResult{
 		Capabilities: capabilities,
 		ServerInfo: &protocol.InitializeResultServerInfo{
-			Name:    "ruby-ti",
+			Name:    "ruby-ti-lsp",
 			Version: &[]string{"beta"}[0],
 		},
 	}, nil
