fb3b0dd0bb208fc965e37f6c3f3573a8ba3e90a5
hamachan <yusuke9127@gmail.com>
2025-10-26 08:05:50 +0900

    fix: type infference

--- test/f2af887e_test.go
+++ test/f2af887e_test.go
@@ -11,7 +11,7 @@ func TestF2af887e(t *testing.T) {
 
 	output, _ := cmd.CombinedOutput()
 
-	expectedOutput := "./f2af887e.rb:::7:::type mismatch: expected Union<Integer Float>, but got String for test"
+	expectedOutput := "./f2af887e.rb:::4:::type mismatch: expected Union<Integer Float>, but got String for Integer.+"
 
 	if strings.TrimSpace(string(output)) != strings.TrimSpace(expectedOutput) {
 		t.Errorf("Expected output: %s, but got: %s", expectedOutput, string(output))
