e2b31728e49b999f474cb81f52cc12606a1bb261
hamachan <yusuke9127@gmail.com>
2025-11-14 08:54:45 +0900

    add: tests

---
+++ test/3ef3d375.rb
@@ -0,0 +1,11 @@
+dbtp Test.compact_array ['1']
+
+dbtp Test.compact_complex ['1']
+
+dbtp Test.compact_union 1
+dbtp Test.compact_union []
+
+dbtp Test.compact_asterisk 1, 2, 3
+
+dbtp Test.compact_optional
+dbtp Test.compact_optional '1'
---
+++ test/3ef3d375_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"os/exec"
+	"strings"
+	"testing"
+)
+
+func Test3ef3d375(t *testing.T) {
+	cmd := exec.Command("../ti", "./3ef3d375.rb")
+
+	output, _ := cmd.CombinedOutput()
+
+	expectedOutput := `./3ef3d375.rb:::1:::Array<String>
+./3ef3d375.rb:::3:::Union<Array<String> Nil>
+./3ef3d375.rb:::5:::Union<String Integer>
+./3ef3d375.rb:::6:::type mismatch: expected Union<String Integer>, but got Array<untyped> for Test.compact_union
+./3ef3d375.rb:::6:::Array<untyped>
+./3ef3d375.rb:::8:::String
+./3ef3d375.rb:::10:::String
+./3ef3d375.rb:::11:::String`
+
+	if strings.TrimSpace(string(output)) != strings.TrimSpace(expectedOutput) {
+		t.Errorf("Expected output: %s, but got: %s", expectedOutput, string(output))
+	}
+}
