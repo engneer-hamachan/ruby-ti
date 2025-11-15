a7200834173f06be627f7cce0061bcff4d9ecb4c
hamachan <yusuke9127@gmail.com>
2025-11-16 06:25:40 +0900

    add: lambda alias

---
+++ test/d29ae395.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base
+  dbtp scope :a, -> {}
+  scope :a, 1
+end
+
+
---
+++ test/d29ae395_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"os/exec"
+	"strings"
+	"testing"
+)
+
+func TestD29ae395(t *testing.T) {
+	cmd := exec.Command("../ti", "./d29ae395.rb")
+
+	output, _ := cmd.CombinedOutput()
+
+	expectedOutput := `./d29ae395.rb:::2:::Nil
+./d29ae395.rb:::3:::type mismatch: expected Proc, but got Integer for User.scope`
+
+	if strings.TrimSpace(string(output)) != strings.TrimSpace(expectedOutput) {
+		t.Errorf("Expected output: %s, but got: %s", expectedOutput, string(output))
+	}
+}
