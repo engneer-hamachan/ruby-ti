497c0490f76518d5e9758e7e3f0559b697b99454
hamachan <yusuke9127@gmail.com>
2025-12-21 08:41:35 +0900

    fix: lexer read token bug

--- lexer/reader/reader.go
+++ lexer/reader/reader.go
@@ -2,18 +2,24 @@ package reader
 
 import (
 	"bufio"
+	"io"
 )
 
 type LexerReader struct {
-	reader   bufio.Reader
+	runes    []rune
+	pos      int
 	ungetFlg bool
 	char     rune
 	history  []rune
 }
 
 func New(r bufio.Reader) LexerReader {
+	content, _ := io.ReadAll(&r)
+	runes := []rune(string(content))
+
 	return LexerReader{
-		reader:   r,
+		runes:    runes,
+		pos:      0,
 		ungetFlg: false,
 	}
 }
@@ -21,18 +27,22 @@ func New(r bufio.Reader) LexerReader {
 func (lr *LexerReader) Read() rune {
 	if lr.ungetFlg {
 		lr.ungetFlg = false
-
 		return lr.char
 	}
 
 	if len(lr.history) > 0 {
 		lr.char = lr.history[0]
 		lr.history = lr.history[1:]
-
 		return lr.char
 	}
 
-	lr.char, _, _ = lr.reader.ReadRune()
+	if lr.pos >= len(lr.runes) {
+		lr.char = 0
+		return 0
+	}
+
+	lr.char = lr.runes[lr.pos]
+	lr.pos++
 
 	return lr.char
 }
